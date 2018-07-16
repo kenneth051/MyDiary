586a228caaf34702b7e3f385311c3243dd480d6f
kenneth051 <kjdumba@gmail.com>
2018-07-16 05:51:03 -0700

    Adding changes to UI files

--- UI/index.js
+++ UI/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function login(){
 	window.location="login.html";
 }
@@ -29,35 +28,3 @@ function update(){
 function reminders(){
 	window.location="Reminders.html";
 }
-=======
-function login(){
-	window.location="login.html";
-}
-function register(){
-	window.location="index.html";
-}
-function home(){
-	document.location="home.html";
-}
-function profile(){
-	window.location="profile.html";
-}
-function editprofile(){
-	window.location="EditProfile.html";
-}
-function allentries(){
-	window.location="AllEntries.html";
-}
-function single(){
-	window.location="SingleEntry.html";
-}
-function logout(){
-	window.location="login.html";
-}
-function update(){
-	window.location="UpdateEntry.html";
-}
-function reminders(){
-	window.location="Reminders.html";
-}
->>>>>>> 664d925a521a1659819fed35a18cb116124b5f8d
